a15ef683d2e5dcd8c680cedf9d20f7ec933c06fa
AJARTCGA <ajartcga@gmail.com>
2019-07-17 23:34:50 -0400

    Pushing up correct 0.5, 0.9, and single focal org data files.

--- data_gen_mod.r
+++ data_gen_mod.r
@@ -1,9 +1,13 @@
 rm(list=ls())
+ONLY_ONE = T
 for(num_tests in c(10,20)){
   for(pop_size in c(10,20,100)){
-    for(pass_prob in c(1, 0.1, 0.01, 0.9, 0.5)){
+    for(pass_prob in c(0.9, 0.5, 0.1, 0.01, 1)){
       for(rep_id in 1:100){
-        num_non_focal_orgs = pop_size - 1#0.9 * pop_size
+        num_non_focal_orgs = pop_size * 0.9
+        if(ONLY_ONE){
+          num_non_focal_orgs = pop_size - 1
+        }
         tmp_vec = rep(0, num_non_focal_orgs)
         data = data.frame(data = matrix(nrow = 0, ncol = num_tests + 1))
         num_focal_orgs = pop_size - num_non_focal_orgs
@@ -21,7 +25,12 @@ for(num_tests in c(10,20)){
         col_names = c('Fitness', paste('Test_',1:(num_tests-1)), 'Focal')
         colnames(data) = col_names
         data$Fitness <- rowSums(data,na.rm = F,dims = 1L)
-        write.csv(data, file = paste("Population_Single_", num_tests, "_", pop_size, "_", pass_prob, "_", rep_id,".csv",sep=""))
+        if(ONLY_ONE){
+          write.csv(data, file = paste("Population_Single_", num_tests, "_", pop_size, "_", pass_prob, "_", rep_id,".csv",sep=""))
+        }
+        else{
+          write.csv(data, file = paste("Population_", num_tests, "_", pop_size, "_", pass_prob, "_", rep_id,".csv",sep=""))
+        }
       }
     }
   }
